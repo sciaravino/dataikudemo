506ea0e6df8098699ef1f62208af1eda80d16d9a
admin <sal.ciaravino17@gmail.com>
2021-10-31 21:12:40 +0000

    Created python recipe: INSURANCE.recipe_from_notebook_admins_Python_notebook_on_insurance_customer_prepared

---
+++ recipes/recipe_from_notebook_admins_Python_notebook_on_insurance_customer_prepared.py
@@ -0,0 +1,26 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE_MAGIC_CELL
+# Automatically replaced inline charts by "no-op" charts
+# %pylab inline
+import matplotlib
+matplotlib.use("Agg")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import dataiku
+from dataiku import pandasutils as pdu
+import pandas as pd
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Read the dataset as a Pandas dataframe in memory
+# Note: here, we only read the first 100K rows. Other sampling options are available
+dataset_insurance_customer_prepared = dataiku.Dataset("insurance_customer_prepared")
+df = dataset_insurance_customer_prepared.get_dataframe(limit=100000)
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Get some simple descriptive statistics
+pdu.audit(df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Recipe outputs
+insurance_customer_modeling = dataiku.Dataset("insurance_customer_modeling")
+insurance_customer_modeling.write_with_schema(pandas_dataframe)
